a9f87cb5d115f822ff0fb4c7230b8540d242bbad
Abhinash <abhinashjangapelly@gmail.com>
2026-09-02 12:08:49 +0530

    Configure Metro for monorepo

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,6 +1,22 @@
+const path = require("path")
 const { getDefaultConfig } = require("expo/metro-config")
 const { withNativeWind } = require("nativewind/metro")
 
-const config = getDefaultConfig(__dirname)
+const projectRoot = __dirname
+const workspaceRoot = path.resolve(projectRoot, "../..")
+
+const config = getDefaultConfig(projectRoot)
+
+// Watch all files within the monorepo
+config.watchFolders = [workspaceRoot]
+
+// Let Metro know where to resolve packages
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+]
+
+// Force Metro to resolve dependencies from the nodeModulesPaths
+config.resolver.disableHierarchicalLookup = true
 
 module.exports = withNativeWind(config, { input: "./global.css" })
